d1b0665f3a930542cd2ea730a5603838772f01bb
Caro407 <caro.lefevre407@hotmail.fr>
2021-06-08 15:19:10 +0200

    set up next.config file for local/prod environments

--- next.config.js
+++ next.config.js
@@ -1,5 +1,17 @@
-module.exports = {
-  env: {
-    url: 'http://localhost:3000/',
-  },
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
+
+module.exports = (phase, { defaultConfig }) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      env: {
+        url: 'http://localhost:3000/',
+      }
+    }
+  }
+
+  return {
+    env: {
+      url: 'https://thp-immo-project-stage.herokuapp.com/',
+    }
+  }
 }
