56e9c2f963893f75ec6c382bbcc0f5a7df2406fd
Ariane <pikabooariane@hotmail.fr>
2021-06-09 11:45:12 +0300

    fixed fetch url and changed postcard format

--- components/CardIndex.js
+++ components/CardIndex.js
@@ -2,7 +2,6 @@ import Link from "next/link";
 import styles from '../styles/Home.module.css'
 
 const CardIndex = (props) => {
-
     return (
       <div className="rounded p-4 m-4 bg-red-100">
         <h2 className="font-bold text-lg text-red-500 capitalize text-center">{props.title}</h2>
---
+++ pages/posts/[id]/index.js
@@ -0,0 +1,34 @@
+import PostCard from "../../../components/PostCard";
+
+export const getServerSidePaths = async () => {
+    const res = await fetch(`${process.env.url}/posts`)
+    const data = await res.json();
+
+    const paths = data.map(post => {
+        return {
+            params: {id: post.id.toString()}
+        }
+    })
+
+    return {
+        paths,
+        fallback: false
+    }
+}
+
+export const getServerSideProps = async (context) => {
+    const id = context.params.id;
+    const res = await fetch(`${process.env.url}/posts/` + id);
+    const data = await res.json();
+    return {
+        props: {post: data}
+    }
+}
+
+const Post = ({post}) => {   
+    return (
+      <PostCard title={post.title} content={post.content} price={post.price} owner={post.owner_id}/>
+    )
+}
+
+export default Post
