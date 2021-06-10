19336f4229d0352c8d165231e44b787e805c2319
Ariane <pikabooariane@hotmail.fr>
2021-06-10 16:31:10 +0300

    fixed form and navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -39,11 +39,6 @@ const Navbar = (props) => {
                     }   w-full lg:inline-flex lg:flex-grow lg:w-auto`}
             >
                 <div className='lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start  flex flex-col lg:h-auto'>
-                    <Link href='/'>
-                        <a className={styles.navbarLink}>
-                            Accueil
-                        </a>
-                    </Link>
                     {props.navbarElements}
                 </div>
             </div>
